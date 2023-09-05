ce615933925591ce35e4c12b8cbb8fc87816cca3
moseoh <azqazq195@gmail.com>
2023-09-05 18:22:29 +0900

    feat: fcm client 구현 - message 전송 - message 이력 조회

---
+++ src/main/java/wanted/structure/Instagram_clone/messages/entity/Notification.java
@@ -0,0 +1,12 @@
+package wanted.structure.Instagram_clone.messages.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+
+    private String title;
+    private String body;
+}
