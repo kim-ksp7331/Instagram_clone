b2a5674e31e531800eb9e0f8fdb5bc4b3a425b66
nayu99 <nyj3230@naver.com>
2023-09-06 15:09:52 +0900

    feat : 이메일 인증 시 유효성 검사
    - 이메일 인증 시 이메일 유효성 검사

--- src/main/java/wanted/structure/Instagram_clone/api/auth/dto/request/EmailRequest.java
+++ src/main/java/wanted/structure/Instagram_clone/api/auth/dto/request/EmailRequest.java
@@ -1,5 +1,7 @@
 package wanted.structure.Instagram_clone.api.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmailRequest {
-
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
+        message = "이메일 형식이 맞지 않습니다.")
     private String email;
 
 }
