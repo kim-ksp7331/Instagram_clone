82c5db4c8feb1828b69c4991cd3b6402b6ee1de0
nayu99 <nyj3230@naver.com>
2023-09-06 16:14:56 +0900

    refac : Mapper 라이브러리 사용
    - UserMapper mapstruct 라이브러리로 변경

--- src/main/java/wanted/structure/Instagram_clone/api/user/mapper/UserMapper.java
+++ src/main/java/wanted/structure/Instagram_clone/api/user/mapper/UserMapper.java
@@ -1,17 +1,11 @@
 package wanted.structure.Instagram_clone.api.user.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 import wanted.structure.Instagram_clone.api.auth.dto.request.SignUpRequest;
 import wanted.structure.Instagram_clone.api.user.entity.User;
 
-@Component
-public class UserMapper {
 
-    public User dtoToEntity(SignUpRequest dto) {
-        return User.builder()
-            .email(dto.getEmail())
-            .password(dto.getPassword())
-            .nickname(dto.getNickname())
-            .build();
-    }
+@Mapper(componentModel="spring")
+public interface UserMapper {
+    User dtoToEntity(SignUpRequest dto);
 }
