6fc6ad4ab8556aa71e4b7e06327492e49b2008fa
nayu99 <nyj3230@naver.com>
2023-09-07 16:17:54 +0900

    test : authService 테스트
    - 회원 가입 테스트
    - UserMapper 회원 가입시 id는 ignore

--- src/test/java/wanted/structure/Instagram_clone/auth/AuthServiceTest.java
+++ src/test/java/wanted/structure/Instagram_clone/auth/AuthServiceTest.java
@@ -9,15 +9,34 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import wanted.structure.Instagram_clone.api.auth.dto.request.SignUpRequest;
 import wanted.structure.Instagram_clone.api.auth.dto.request.VerifyMailRequest;
 import wanted.structure.Instagram_clone.api.auth.dto.response.VerifyMailResponse;
+import wanted.structure.Instagram_clone.api.auth.entity.Auth;
+import wanted.structure.Instagram_clone.api.auth.mapper.AuthMapper;
+import wanted.structure.Instagram_clone.api.auth.repository.AuthRepository;
 import wanted.structure.Instagram_clone.api.auth.service.AuthService;
+import wanted.structure.Instagram_clone.api.user.entity.User;
+import wanted.structure.Instagram_clone.api.user.mapper.UserMapper;
+import wanted.structure.Instagram_clone.api.user.repository.UserRepository;
+import wanted.structure.Instagram_clone.global.code.AuthCode;
 import wanted.structure.Instagram_clone.global.code.RedisCode;
 import wanted.structure.Instagram_clone.global.utils.RedisUtils;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private AuthRepository authRepository;
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private AuthMapper authMapper;
 
     @Mock
     private RedisUtils redisUtils;
@@ -43,6 +62,39 @@ public class AuthServiceTest {
 
         //then
         Assertions.assertEquals(response.isVerify(), test.isVerify());
+
+    }
+
+    @Test
+    void signUp() {
+        //given
+        Long userId = 1L;
+        Long authId = 1L;
+
+        SignUpRequest request = SignUpRequest.builder()
+            .email("test1@test.com")
+            .password("test_password")
+            .nickname("test_nickname")
+            .build();
+
+        User user = User.builder().email("test1@test.com")
+            .password("test_password")
+            .nickname("test_nickname")
+            .id(userId)
+            .build();
+
+        Auth auth = Auth.builder().authId(authId).userId(userId).role(AuthCode.USER.getCode()).build();
+
+        given(userRepository.existsByEmail(request.getEmail())).willReturn(false);
+        given(userRepository.save(userMapper.dtoToEntity(request))).willReturn(user);
+        given(authRepository.save(authMapper.dtoToEntity(user.getId(), AuthCode.USER))).willReturn(auth);
+
+        //when
+        Long testId = authService.signUp(request);
+        log.info("signUp testId - {}", testId);
+
+        //then
+        Assertions.assertEquals(user.getId(), testId);
 
     }
 }
